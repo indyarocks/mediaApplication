9d3cf403682ca9bc96553712338c7ac2e7129c7f
ChandanKumar <chandan@idyllic-software.com>
2015-03-28 23:06:37 +0530

    contact route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'help' => 'home#help'
-  get 'about' => 'home#help'
+  get 'about' => 'home#about'
   get 'contact' => 'home#contact'
   resources :sessions, only: [:new, :create, :destroy]
   get '/signup' => "users#new"
