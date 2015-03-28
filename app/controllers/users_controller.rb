1a73cf4496bb01ca759bac3a3e0c35f6c919c3de
ChandanKumar <chandan@idyllic-software.com>
2015-03-28 23:03:34 +0530

    contact form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @new_media = current_user.medias.build
     params[:q] ||= '%'
     @media_permissions = Media::Media::PERMISSION
-    @medias = @user.medias.where('description LIKE ?', "#{params[:q]}%").paginate(:page => params[:page], :per_page => 2)
+    @medias = @user.medias.where('description LIKE ?', "#{params[:q]}%").paginate(:page => params[:page], :per_page => 30)
   end
 
   def new
